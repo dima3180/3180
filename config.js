43cbbe26f2626b1b28b5c7e5777afbb5eca5d9cb
dima3180 <75124591+dima3180@users.noreply.github.com>
2020-11-28 13:32:29 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const TOKEN = "98d75da5a3f9090a25f0921a45203f2e661e47c9a6063eeac9f2ae904091cabcf125050f9a9d792a60b98"; // Токен группы, в которой будет стоять бот
+const GROUP_ID = 168024722; // Айди группы, в которой будет стоять бот
+const BTN_TEXT = ["raid"];
+const TIME = 1000000;
+const CHAT_SPAM = ["[id459080420|$£÷]"]
+const HELLO_TEXT = `Пригласи меня в конфу и будет прикол
+`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
